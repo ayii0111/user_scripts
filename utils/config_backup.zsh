b31ccd8f33ffcebdbb046918116eb7bff604089c
ayii <a0913873707@gmail.com>
2023-12-15 15:35:13 +0800

    設定檔拷貝腳本添加snippets

--- utils/config_backup.zsh
+++ utils/config_backup.zsh
@@ -1,7 +1,8 @@
 #! /bin/zsh
 
 # 透過自製的 mp指令，將設定檔備份管理
-mp $HOME/.zshrc ~/user_config/
-mp "$HOME/Library/Application Support/Code/User/settings.json" ~/user_config/Code
-mp "$HOME/Library/Application Support/Code/User/keybindings.json" ~/user_config/Code
-mp $HOME/.fig/settings.json ~/user_config/fig
+mp $HOME/.zshrc $HOME/user_config/
+mp "$HOME/Library/Application Support/Code/User/settings.json" $HOME/user_config/Code
+mp "$HOME/Library/Application Support/Code/User/keybindings.json" $HOME/user_config/Code
+mp $HOME/.fig/settings.json $HOME/user_config/fig
+mp $HOME/Library/Application\ Support/Code/User/snippets/*  $HOME/user_config/snippets
