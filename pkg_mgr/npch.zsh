e9569a7595aa9aa7d439f7d5e13a41864e95a5a4
ayii <a0913873707@gmail.com>
2024-06-26 14:50:51 +0800

    完成了 npch 腳本：可用來使用指令快速檢查當前專案是否安裝了某一軟件

--- pkg_mgr/npch.zsh
+++ pkg_mgr/npch.zsh
@@ -2,22 +2,25 @@
 
 # 確認是否有傳入參數
 if [ $# -eq 0 ]; then
-  echo "請提供套件名稱"
-  exit 1
+  echo "錯誤：請提供套件名稱" >&2
+  return 1
 fi
 
 # 套件名稱
-PACKAGE_NAME=$1
+local PACKAGE_NAME=$1
+local VERSION
+local PACKAGE_INFO
 
 # 使用 npm list 檢查套件是否已安裝
 PACKAGE_INFO=$(npm list $PACKAGE_NAME 2>/dev/null | grep $PACKAGE_NAME)
 
 # 判斷套件是否已安裝
 if [ -z "$PACKAGE_INFO" ]; then
-  echo "$PACKAGE_NAME 沒有安裝"
+  echo "沒有安裝 $PACKAGE_NAME"
 else
   # 從輸出中提取版本信息
   VERSION=$(echo $PACKAGE_INFO | grep -o '@[^ ]*' | tail -1)
-  echo "$PACKAGE_NAME$VERSION"
+  echo "已安裝 $PACKAGE_NAME$VERSION"
 fi
 
+unset PACKAGE_NAME VERSION PACKAGE_INFO
