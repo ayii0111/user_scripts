ed536e4963749bfcd415e91d68ed4e5f3b249337
ayii <a0913873707@gmail.com>
2024-10-02 18:53:53 +0800

    改善 vlodash 腳本

--- vite/eslim.zsh
+++ vite/eslim.zsh
@@ -3,7 +3,7 @@
 # eslim 腳本
 # 可以將統一的 eslint 設定檔覆蓋到專案中
 sourceFile=$HOME/UserConfig/config/eslint.config.mjs
-targetFile=./eslintrc.mjs
+targetFile=./eslint.config.mjs
 cfsyn $sourceFile $targetFile
 
 unset sourceFile targetFile
--- vite/vlodash.zsh
+++ vite/vlodash.zsh
@@ -7,45 +7,45 @@ npm i lodash-es
 # 安裝型別
 npm i -D @types/lodash-es
 
-# lodash廢棄判斷插件
-# npm i -D eslint-plugin-you-dont-need-lodash-underscore
-
-
-# 下面兩種格式，可以透過路徑展開匹配，就不需要使用 if 判斷式了
-# 適用於 flat 新式 eslint 設定檔的標準格式
-# local eslintImport="import { udnLodash } from 'eslint-plugin-you-dont-need-lodash-underscore'"
-# gsed -i "/^$/ d" eslint.config*
-# gsed -i "0,/export default/ {// s|^|$eslintImport\n\n|}" eslint.config.*
-# local eslintPluginConfig="
-#   {
-#     ...udnLodash
-#   },"
-# eslintPluginConfig=$(echo "$eslintPluginConfig" | gsed ':a;N;$!ba;s/\n/\\n/g')
-# gsed -i "0,/^  },/ {// s|$|$eslintPluginConfig|}" eslint.config.*
-
-
-# 適用於舊式 eslint 設定檔
-# eslintPluginConfig=",
-#     'plugin:you-dont-need-lodash-underscore/compatible'"
-
-# eslintPluginConfig=$(echo "$eslintPluginConfig" | gsed ':a;N;$!ba;s/\n/\\n/g')
-# gsed -i "/extends*.: /,$ { 0,/'$/ {// s|$|$eslintPluginConfig|}}" ./.eslintrc.*
-
-
-
-
-
+# ------------------------------ vite.config 修改 ------------------------------
 # 以下為 auto-import 的配置
 local autoImportConfig=",
         {'lodash-es': ['cloneDeep', 'camelCase', 'isPlainObject', 'merge']}"
 
 autoImportConfig=$(echo "$autoImportConfig" | gsed ':a;N;$!ba;s/\n/\\n/g')
-for file (./vite.config.{t,j}s) {
-  if [[ -f $file ]] {
-  # 在搜尋時，若有[] 需轉譯
-  gsed -i "/imports: \[/,$ { 0,/\}$/ {// s|$|$autoImportConfig|}}" $file
-  }
-}
+for file in ./vite.config.{t,j}s; do
+  if [[ -f $file ]]; then
+    # 在搜尋時，若有[] 需轉譯
+    sed -i "/imports: \[/,/\]/ s/\]$/,$autoImportConfig]/" "$file"
+  fi
+done
 
+# ------------------------------ package.json 修改 ------------------------------
+# 檢查是否存在 package.json
+if [[ ! -f package.json ]]; then
+  echo "package.json 文件不存在，請檢查後重試。"
+  return 1
+fi
+
+# 讀取當前的 type 屬性值
+current_type=$(gsed -n 's/.*"type"[ ]*:[ ]*"\(.*\)".*/\1/p' package.json | head -n 1)
+
+# 檢查是否有找到 type 屬性
+if [[ -z "$current_type" ]]; then
+  # 若不存在，則新增 type: "module"
+  gsed -i '1,/\{/s/\{/\{\n  "type": "module",/' package.json
+else
+  # 若存在且不是 'module'，則輸出當前值並詢問是否覆蓋
+  if [[ "$current_type" != "module" ]]; then
+    echo "當前 'type' 值為: $current_type"
+    read -p "是否要覆蓋為 'module'？ (y/n): " answer
+    if [[ "$answer" == "y" ]]; then
+      gsed -i 's/"type"[ ]*:[ ]*"[^"]*"/"type": "module"/' package.json
+      echo "'type' 屬性已更新為 'module'。"
+    else
+      echo "保留當前的 'type' 值：$current_type"
+    fi
+  fi
+fi
 
 unset eslintImport eslintPluginConfig autoImportConfig
